062729c8c0ca7c8fce2e58305bd7696fda1f42c7
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-07-25 01:13:39 +0700

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -10,6 +10,18 @@ const { username, room } = Qs.parse(location.search, {
 
 const socket = io();
 
+//new
+$('form').submit(function(){
+          socket.emit('chat message', $('#m').val());
+          $('#m').val('');
+          return false;
+        });
+socket.on('chat message', function(msg){
+          $('#messages').append($('<li>').text(msg));
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+
+//new
 // Join chatroom
 socket.emit('joinRoom', { username, room });
 
